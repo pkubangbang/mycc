9250ad98a71fdcbcc0426fb08e4f3223cec89f05
pkubangbang <pkubangbang@pku.edu.cn>
2026-05-14 23:11:14 +0800

    feat: add /todos clear, done, undone subcommands for todo management

--- src/slashes/todos.ts
+++ src/slashes/todos.ts
@@ -1,14 +1,32 @@
 /**
- * /todos command - Print todo list
+ * /todos command - Print todo list, or clear all todos
+ *
+ * Usage:
+ *   /todos        - Print todo list
+ *   /todos clear  - Clear all todos
  */
 
 import type { SlashCommand } from '../types.js';
+import chalk from 'chalk';
 
 export const todosCommand: SlashCommand = {
   name: 'todos',
-  description: 'Print todo list',
+  description: 'Print todo list (/todos [clear])',
   aliases: ['todo'],
   handler: (context) => {
-    console.log(context.ctx.todo.printTodoList());
+    const subCommand = context.args[1];
+
+    if (subCommand === 'clear') {
+      context.ctx.todo.clear();
+      console.log(chalk.green('All todos cleared.'));
+    } else if (!subCommand) {
+      console.log(chalk.cyan('\n=== Todos ===\n'));
+      console.log(context.ctx.todo.printTodoList());
+      console.log(chalk.gray('\nSubcommands:'));
+      console.log(chalk.gray('  /todos clear   Clear all todos'));
+    } else {
+      console.log(chalk.yellow(`Unknown subcommand: ${subCommand}`));
+      console.log(chalk.gray('Available: clear'));
+    }
   },
 };
